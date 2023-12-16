f69edf1b70fededbec9270b0f916f84a5264e430
ArchitKushmail <architkushmail1@gmail.com>
2023-12-16 23:17:56 +0530

    hfc

--- 1. Beginners/02. Primitives/01 Variables/live coding/app.js
+++ 1. Beginners/02. Primitives/01 Variables/live coding/app.js
@@ -1,7 +1,7 @@
 // (reserved keyword): (variable name): (type of value)
 
 let variable; // undefined
-let name = "HuXn WebDev";
-console.log(name);
+let name1 = "HuXn WebDev";
+console.log(name1);
 const channelName = "WebDev Academy";
 console.log(channelName);
